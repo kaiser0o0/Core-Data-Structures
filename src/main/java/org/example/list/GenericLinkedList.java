a39f59c1c4c77880170ede5eb56b7e8b9abfc750
Muhammet Öztürk <180087422+Kaiser106@users.noreply.github.com>
2026-08-20 12:07:17 +0300

    add

--- src/main/java/org/example/list/GenericLinkedList.java
+++ src/main/java/org/example/list/GenericLinkedList.java
@@ -1,4 +1,46 @@
 package org.example.list;
 
-public class GenericLinkedList {
+public class GenericLinkedList<T> {
+
+
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+
+        public Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node<T> head;
+    private int size;
+
+    public GenericLinkedList() {
+        this.head = null;
+        this.size = 0;
+    }
+
+
+    public void addFirst(T data) {
+        Node<T> newNode = new Node<>(data);
+        newNode.next = head;
+        head = newNode;
+        size++;
+    }
+
+
+    public void add(T data) {
+        Node<T> newNode = new Node<>(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<T> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+    }
 }
