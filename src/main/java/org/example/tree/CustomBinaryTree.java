3405f4f2e805a9a027f0e0b261e76a563d1ba235
Muhammet Öztürk <180087422+Kaiser106@users.noreply.github.com>
2026-08-20 16:42:55 +0300

    add

--- src/main/java/org/example/tree/CustomBinaryTree.java
+++ src/main/java/org/example/tree/CustomBinaryTree.java
@@ -1,4 +1,49 @@
 package org.example.tree;
 
-public class CustomBinaryTree {
+public class CustomBinaryTree<T extends Comparable<T>> {
+
+    private Node<T> root;
+
+    public CustomBinaryTree() {
+        this.root = null;
+    }
+
+    public void insert(T data) {
+        root = insertRecursive(root, data);
+    }
+
+    private Node<T> insertRecursive(Node<T> current, T data) {
+        if (current == null) {
+            return new Node<>(data);
+
+        }
+
+
+        if (data.compareTo(current.data) < 0) {
+            current.left = insertRecursive(current.left, data);
+
+        } else if (data.compareTo(current.data) > 0) {
+            current.right = insertRecursive(current.right, data);
+
+        } else {
+            return current;
+        }
+
+        return current;
+    }
+
+    public void displayInOrder() {
+        System.out.print("Ağaç (In-Order): [ ");
+        traverseInOrder(root);
+        System.out.println("]");
+    }
+
+    private void traverseInOrder(Node<T> node) {
+        if (node != null) {
+            traverseInOrder(node.left);
+            System.out.print(node.data + " ");
+            traverseInOrder(node.right);
+        }
+
+    }
 }
--- src/main/java/org/example/tree/Node.java
+++ src/main/java/org/example/tree/Node.java
@@ -1,4 +1,15 @@
 package org.example.tree;
 
-public class Node {
+public class Node<T extends Comparable<T>> {
+    T data;
+    Node<T> left;
+    Node<T> right;
+
+    public Node(T data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+
+    }
+
 }
